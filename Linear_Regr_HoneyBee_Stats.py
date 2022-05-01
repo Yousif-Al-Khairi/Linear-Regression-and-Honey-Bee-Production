037de6ebb4b590478b9fdb9ad6f537920ae3deac
Yousif <yousifzaid44002@gmail.com>
2022-05-01 09:20:53 +0200

    Added HoneyBee data, accessed it in python, and added required libraries to parse data

--- Linear_Regr_HoneyBee_Stats.py
+++ Linear_Regr_HoneyBee_Stats.py
@@ -1,1 +1,7 @@
-#Initial Commit, Hello World
+#Import all required python libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('honeyproduction.csv')
+
